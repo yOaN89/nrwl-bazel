dcec7b711e629e1f6197e99fa2fe25f5db62b136
Yoan Uria Rodriguez <yrodriguez@tritansoftware.com>
2021-09-20 11:20:18 +0100

    initial commit

---
+++ apps/demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tritanbuild-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'demo';
+}
